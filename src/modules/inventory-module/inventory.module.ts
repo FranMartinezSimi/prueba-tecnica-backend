11ee1da8be31b28e04c15d4b1abeb63ccad82dd8
juan martinez <jua.martinez@outlook.es>
2024-12-06 21:02:02 -0300

    fixed dependencies

--- src/modules/inventory-module/inventory.module.ts
+++ src/modules/inventory-module/inventory.module.ts
@@ -1,20 +1,31 @@
-import { Logger, Module } from '@nestjs/common';
+import { forwardRef, Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Inventory } from '@/entities/Inventory.entity';
 import { InventoryRepository } from './repository/inventory.repository';
 import { InventoryService } from './service/inventory.service';
-import { Perfume } from '@/entities/Perfume.entity';
 import { InventoryController } from './controller/inventory.controller';
+import { PerfumesModule } from '../perfumes-module/perfumes.module';
+import { PerfumesRepository } from '../perfumes-module/repository/perfumes.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Inventory, Perfume])],
-  providers: [InventoryService, 
+  imports: [
+    TypeOrmModule.forFeature([Inventory]),
+    forwardRef(() => PerfumesModule)
+  ],
+  providers: [
+    InventoryService, 
+    InventoryRepository,
     {
       provide: 'INVENTORY_REPOSITORY',
       useClass: InventoryRepository,
-    }, 
-    Logger],
-  exports: [InventoryService, 'INVENTORY_REPOSITORY'],
+    },
+    Logger
+  ],
+  exports: [
+    InventoryService, 
+    'INVENTORY_REPOSITORY',
+    InventoryRepository
+  ],
   controllers: [InventoryController],
 })
 export class InventoryModule {}
--- src/modules/perfumes-module/perfumes.module.ts
+++ src/modules/perfumes-module/perfumes.module.ts
@@ -1,16 +1,31 @@
-import { Module, Logger } from '@nestjs/common';
+import { Module, Logger, forwardRef } from '@nestjs/common';
 import { PerfumesService } from './services/perfumes.service';
 import { PerfumesRepository } from './repository/perfumes.repository';
 import { PerfumesController } from './controller/perfumes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Perfume } from '@/entities/Perfume.entity';
+import { InventoryRepository } from '../inventory-module/repository/inventory.repository';
+import { InventoryModule } from '../inventory-module/inventory.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Perfume])],
-  providers: [PerfumesService, PerfumesRepository, Logger, {
-    provide: 'PERFUMES_REPOSITORY',
-    useClass: PerfumesRepository,
-  }],
+  imports: [
+    TypeOrmModule.forFeature([Perfume]),
+    forwardRef(() => InventoryModule)
+  ],
+  providers: [
+    PerfumesService,
+    PerfumesRepository,
+    {
+      provide: 'PERFUMES_REPOSITORY',
+      useClass: PerfumesRepository,
+    },
+    Logger
+  ],
+  exports: [
+    PerfumesService,
+    'PERFUMES_REPOSITORY',
+    PerfumesRepository
+  ],
   controllers: [PerfumesController],
 })
-export class PerfumesModuleModule {}
+export class PerfumesModule {}
